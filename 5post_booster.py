5036945047e2faada1beffd9385d1e0508d14a7a
lensstilte <lensstilte@gmail.com>
2026-07-15 23:32:04 +0200

    Update 5post_booster.py

--- 5post_booster.py
+++ 5post_booster.py
@@ -10,11 +10,8 @@ BOOST_POSTS = [
     "https://bsky.app/profile/big-dominio.bsky.social/post/3mqp76mglrs2q",
     "https://bsky.app/profile/big-dominio.bsky.social/post/3mqlu4fzbis2c",
     "https://bsky.app/profile/big-dominio.bsky.social/post/3mqj5537wxs2d",
-
 "https://bsky.app/profile/carmenle91.bsky.social/post/3mqkskgovu22h",
-
 "https://bsky.app/profile/carmenle91.bsky.social/post/3mogcwof3sk2v",
-
 "https://bsky.app/profile/carmenle91.bsky.social/post/3mnzp22uwdk2u",
 
 
