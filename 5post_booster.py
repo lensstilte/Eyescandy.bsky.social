cc6b0c3c5d2371adbbe0a279d12fb1dfc489469f
lensstilte <lensstilte@gmail.com>
2026-08-02 16:48:56 +0200

    Update 5post_booster.py

--- 5post_booster.py
+++ 5post_booster.py
@@ -7,12 +7,12 @@ from atproto import Client
 
 BOOST_POSTS = [
     "https://bsky.app/profile/big-dominio.bsky.social/post/3ms34bfz4l22t",
-    "https://bsky.app/profile/carmenle91.bsky.social/post/3ms3bov3lck23",
+    "https://bsky.app/profile/tullageback.bsky.social/post/3ms3pit3kr42c",
     "https://bsky.app/profile/tasha510.bsky.social/post/3mposmwmpr22o",
     "https://bsky.app/profile/brithneykissme.bsky.social/post/3mqwf6qigkc2g",
     "https://bsky.app/profile/thepriapicknave.bsky.social/post/3ms3jvzqchs2n",
     "https://bsky.app/profile/lekkerekontjes69.bsky.social/post/3ms45j3immo2v",
-    "https://bsky.app/profile/tullageback.bsky.social/post/3ms3pit3kr42c",
+    "https://bsky.app/profile/beautyfan.bsky.social/post/3ms4a2lvhc22d",
 
 
 
