4bd4338731634c7561c2a03786e0f3fac3e46c15
lensstilte <lensstilte@gmail.com>
2026-08-09 14:18:02 +0200

    Update 5post_booster.py

--- 5post_booster.py
+++ 5post_booster.py
@@ -8,12 +8,12 @@ from atproto import Client
 BOOST_POSTS = [
     "",
     "",
+    "",
+    "",
     "https://bsky.app/profile/julyakawaii.bsky.social/post/3msgcbzrrp224",
     "https://bsky.app/profile/julyakawaii.bsky.social/post/3msi4pmzdk22y",
-    "",
-    "",
-    "",
     "https://bsky.app/profile/julyakawaii.bsky.social/post/3msl232tmic23",
+    "https://bsky.app/profile/julyakawaii.bsky.social/post/3msnk2cllkk23",
 
 
 
