9b3c4c5c85589b1f5ad3a0bdf63540962d937d60
lensstilte <lensstilte@gmail.com>
2026-08-14 20:36:29 +0200

    Update 5post_booster.py

--- 5post_booster.py
+++ 5post_booster.py
@@ -10,9 +10,9 @@ BOOST_POSTS = [
     "",
     "",
     "",
-    "https://bsky.app/profile/eyescandy.bsky.social/post/3msyn7t3z2c24",
+    "",
     "https://bsky.app/profile/julyakawaii.bsky.social/post/3msv3ipbwts2s",
-    "https://bsky.app/profile/julyakawaii.bsky.social/post/3msspztsyps2n",
+    "https://bsky.app/profile/julyakawaii.bsky.social/post/3mt2536r72c2s",
     "https://bsky.app/profile/julyakawaii.bsky.social/post/3msxsmmyjsk26",
 
 
